3e8ecf60fba5004eef2d62535a3cc161de76a71e
Jules-1948 <schamingjc20@gcc.edu>
2024-03-17 20:59:08 -0400

    A* Done?

--- src/AStar.java
+++ src/AStar.java
@@ -2,6 +2,7 @@
  * @author Julia Schaming
  */
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class AStar {
     
@@ -10,16 +11,24 @@ public class AStar {
      * Numbered comments (Ex. 5. return searchNode) are directly from pseudocode
      * Runs A* algorithm on one instance of a Classic Board shape and prints results according to verbosity setting
      * @param verbosity - sets degree of console output 
-     *                      1 = final result 
-     *                      2 = Initial board && each set of tiles removed in order && final board
+     *                      1 = Final result 
+     *                      2 = Initial board && final board
+     *                      3 = Initial board && each set of tiles removed in order && final board
      */
     private ArrayList<Tile[]> runInstance(int verbosity, Board startingNode){
+        float startTime = System.currentTimeMillis();
+        System.out.printf("Start Time: %.02f ms\n", startTime);
+
         // Print starting board
-        if(verbosity == 2){System.out.println(startingNode);}
+        if(verbosity == 2 || verbosity == 3){
+            System.out.println("\nStarting board -> No tiles removed yet.");
+            System.out.println(startingNode);
+        }
         
         // 1. Initialize fringe to starting state, closed list to empty
         ArrayList<Board> fringe = new ArrayList<Board>();
         ArrayList<Board> closedList = new ArrayList<Board>();
+        Board lastSearchedBoard = startingNode; //used to print last board at the end when verbosity == 2 or 3
         fringe.add(startingNode);
 
         // 2. While fringe is not empty:
@@ -27,6 +36,7 @@ public class AStar {
             // 3. searchNode <- fringe.removeFirst()
             Board searchNode = fringe.get(0);
             fringe.remove(0);
+            lastSearchedBoard = searchNode;
 
             // print path to searchNode
             if(verbosity == 2){
@@ -34,7 +44,7 @@ public class AStar {
                 if(currentNodePath.isEmpty()){
                     System.out.println("\nBelow lists the path of removed tiles from each searchNode");
                 }
-                System.out.print("Remaining tiles: " + searchNode.getExistentTileCount());
+                System.out.print("\nRemaining tiles: " + searchNode.getExistentTileCount());
                 for(Tile[] tilePair : currentNodePath){
                     System.out.print(" " + tilePair[0].toString() + tilePair[1].toString());
                 }
@@ -42,9 +52,13 @@ public class AStar {
             }
 
             // 4. if isGoal(searchNode.endingState):
-            if(searchNode.getTiles().isEmpty()){
+            if(searchNode.getExistentTileCount() == 0){
                 // Print ending board
-                if(verbosity == 2){System.out.println(searchNode);}
+                System.out.println("\nFinal board searched -> this board is the goal state.");
+                System.out.println(searchNode);
+                System.out.println("\nPath to this board: " + searchNode.getPath());
+                float endTime = System.currentTimeMillis();
+                System.out.printf("Time taken: %.02f ms\n", (endTime-startTime));
 
                 // 5. return searchNode
                 return (searchNode.getPath());
@@ -54,76 +68,80 @@ public class AStar {
             closedList.add(searchNode);
 
             // 7. children <- expand(searchNode)
-            ArrayList<Tile[]> tilePairs = new ArrayList<>();
-            for(Tile tile1 : searchNode.getExposedTiles()){
-                for(Tile tile2 : searchNode.getExposedTiles()){
-                    if(searchNode.canRemoveTiles(tile1, tile2)){
-                        tilePairs.add(new Tile[]{tile1, tile2});
-                    }
-                }
-            }
-
-            // Make a list of children for searchNode
-            ArrayList<Board> children = new ArrayList<>();
-            for(Tile[] child : tilePairs){
-                Board childBoard = searchNode.deepCopy();
-                children.add(childBoard);
-                childBoard.removeTiles(child[0], child[1]);
-                childBoard.addToPath(child[0], child[1]);
-            }
-
+            ArrayList<Board> children = getChildren(searchNode);
 
             // 8. for each child in children:
             for(Board child : children){
-                System.out.print(" " + child.getPath().get(0)[0].toString() + child.getPath().get(0)[1].toString());
                 // 9. if child.endingState is in closedList: continue;
-                boolean containsChild = false;
-                for(Board closed : closedList){
-                    if(child.areEqualBoards(closed)){
-                        containsChild = true;
-                        break;
-                    }
-                }
-                if(containsChild){continue;}
-            
+                if(isInList(child, closedList)){continue;}
 
-                for(Board node : fringe){
-                    // 10. if there is a search node in fringe that ends in child.endingState:
-                    if(child.getExistentTileCount() == node.getExistentTileCount()){
-                        // 11. update fringe to contain only the better of the two paths
-                        // Heuristics: 1. depth -> the few layers the better; outranks 2nd heuristic
-                        //             have 2nd heuristic bc depths will be largely similar
-                        //             2. numExposesd tiles -> more exposed tiles the better
-                        if(child.getDepth() == node.getDepth()){
-                            if(child.getExposedTiles().size() > node.getExposedTiles().size()){
-                                //child is better
-                                fringe.remove(node);
-                                fringe.add(child);
-                            } // else previous node is better
-                        } else if (child.getDepth() > node.getDepth()){
-                            //child is better
-                            fringe.remove(node);
-                            fringe.add(child);
-                        } // else previous node is better
-                    }
-                    // 12. else: 
-                    else{
-                        // 13. add child to fringe
-                        fringe.add(child);
-                    }
-                }
 
-                System.out.println("NO");
-                // In case fringe was empty and the for loop never ran
-                if(fringe.isEmpty()){
+                // 10. if there is a search node in fringe that ends in child.endingState:
+                if(isInList(child, fringe)){
+                    // 11. update fringe to contain only the better of the two paths
+                    // Since nodes in the same endstate are exactly equal, it doesn't matter which we keep
+                    // so we will keep the node already in the fringe
+                }
+                // 12. else: 
+                else{
+                    // 13. add child to fringe
                     fringe.add(child);
                 }
             }
-            System.out.println("here");
-
+            // Sort queue so that best child node is first
+            Collections.sort(fringe);
         }
 
-        return null;
+        System.out.println("\nFinal board searched -> no more moves remaining and goal state is unreachable.");
+        System.out.println(lastSearchedBoard);
+        System.out.println("\nPath to this board: " + lastSearchedBoard.getPath());
+        float endTime = System.currentTimeMillis();
+        System.out.printf("Time taken: %.02f ms\n", (endTime-startTime));
+        return lastSearchedBoard.getPath();
+    }
+
+    // Checks if given board is already in the given list
+    private boolean isInList(Board board, ArrayList<Board> list){
+        boolean containsChild = false;
+        for(Board node : list){
+            if(board.areEqualBoards(node)){
+                containsChild = true;
+                break;
+            }
+        }
+        return containsChild;
+    }
+
+    // Creates array of all children boards given a parent board
+    private ArrayList<Board> getChildren(Board searchNode){
+        ArrayList<Tile[]> tilePairs = getRemovableTilePairs(searchNode);
+        ArrayList<Board> children = new ArrayList<>();
+        for(Tile[] child : tilePairs){
+            Board childBoard = searchNode.deepCopy();
+            children.add(childBoard);
+            childBoard.removeTiles(child[0], child[1]);
+            childBoard.addToPath(child[0], child[1]);
+        }
+        return children;
+
+    }
+
+    // Finds all unique pairs of removable tile pairs for given board
+    private ArrayList<Tile[]> getRemovableTilePairs(Board board) {
+        ArrayList<Tile> exposedTiles = board.getExposedTiles();
+        
+        ArrayList<Tile[]> tilePairs = new ArrayList<>();
+        for(int i = 0; i < exposedTiles.size(); i++){
+            for(int j = i + 1; j < exposedTiles.size(); j++ ){
+                Tile tile1 = exposedTiles.get(i);
+                Tile tile2 = exposedTiles.get(j);
+                if(board.canRemoveTiles(tile1, tile2)){
+                    tilePairs.add(new Tile[]{tile1, tile2});
+                }
+            }
+        }
+
+        return tilePairs;
     }
 
 
